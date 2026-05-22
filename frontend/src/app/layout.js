c1fb1bc937ab4a05d4fb092223cb127d8fda8176
Shubham Kumar <shubham10809@gmail.com>
2026-05-22 15:24:59 +0530

    add a landing page

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -23,9 +23,10 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
